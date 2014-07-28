9088824e1f71f22882ef8b3e53575ab6e9d31c25
Lou Huang <lou@codeforamerica.org>
2014-07-28 09:01:39 -0400

    Update everything: deprecate SASS/Bourbon and use Stylus. Remove unused stuff. Set up for Jade + Polymer? Add Favicon

--- public/js/main.js
+++
@@ -1,7 +0,0 @@
-
-$(document).ready( function () {
-/*
-	var i = Math.floor(Math.random() * 10000)
-	$('header h2').html('System Monitor ' + i)
-*/
-})
--- server.js
+++ server.js
@@ -1,23 +1,45 @@
-var PORT     = 4747,
-	express  = require('express'),
-	path     = require('path'),
-	sass     = require('node-sass')
+var PORT          = 4747,
+    express       = require('express'),
+    path          = require('path'),
+    autoprefixer  = require('autoprefixer-stylus')
+    jade          = require('jade'),
+    morgan        = require('morgan'),
+    stylus        = require('stylus')
 
-var	app = express()
+var app = express()
 
-app.configure(function () {
-	app.set('port', process.env.PORT || PORT)
-	app.use(express.static(path.join(__dirname, 'public')))
-/*	app.use(
-		sass.middleware({
-			src: __dirname + '/src',
-			dest: __dirname + '/public',
-			debug: true,
-			outputStyle: 'compressed'
-		})
-	)*/
-})
+app.set('port', process.env.PORT || PORT)
+app.set('views', __dirname + '/src/views')
+app.set('view engine', 'jade')
+app.engine('jade', require('jade').__express);
+app.use(morgan('dev'))
+
+app.use(
+  stylus.middleware({
+    src: __dirname + '/src',
+    dest: __dirname + '/public',
+    compile: styles
+  }))
+
+app.use(express.static(__dirname + '/public'))
 
 app.listen(app.get('port'), function () {
-	console.log('Listening on port ' + app.get('port'))
+    console.log('Starfleet Library Computer Access and Retrieval System activated at port ' + app.get('port'))
 })
+
+/*
+  Function for additional tasks to perform
+  while compiling stylus files. It will
+  autoprefix the output and then include
+  static vendor CSS files. The path must
+  be set for Node.js (not Stylus) to know
+  where to look for the imported stylesheets.
+*/
+function styles (str, path) {
+  console.log('Compiling stylus')
+  return stylus(str)
+    .use(autoprefixer('last 2 versions'))
+    .set('compress', true)
+    .set('paths', [__dirname + '/src/stylesheets/'])
+    .set('include css', true)
+}
